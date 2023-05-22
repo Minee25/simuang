4805fd58e82fe6bc63b836c65cb1639d482696f7
Minee <78191030+ThaksinMuansuk@users.noreply.github.com>
2023-05-22 06:14:12 -0700

    Add files via upload

---
+++ assets/js/navbar.js
@@ -0,0 +1,12 @@
+// window.onscroll = function () { scrollFunction() };
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//         document.getElementById("navbar").style.background = "#343A41";
+//     } else {
+//         document.getElementById("navbar").style.background = "#343a415d";
+//     }
+// }
+
+// ==================== Responsive ====================
+
